7247a856684511d1a6817087427489c4ca5acced
Sador Kassa <sadorkassa21@gmail.com>
2021-10-25 05:13:33 -0700

    sorting_algorithms

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,76 +1,36 @@
 #include "sort.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * partition - finds the partition for the quicksort using the Hoare scheme
- * @array: array to sort
- * @lo: lowest index of the partition to sort
- * @hi: highest index of the partition to sort
- * @size: size of the array
+ * quick_sort_hoare - function sorts array of ints using quick sort method
  *
- * Return: index of the partition
- */
-size_t partition(int *array, ssize_t lo, ssize_t hi, size_t size)
-{
-	int swap, pivot;
-
-	pivot = array[hi];
-	while (lo <= hi)
-	{
-		while (array[lo] < pivot)
-			lo++;
-		while (array[hi] > pivot)
-			hi--;
-		if (lo <= hi)
-		{
-			if (lo != hi)
-			{
-				swap = array[lo];
-				array[lo] = array[hi];
-				array[hi] = swap;
-				print_array(array, size);
-			}
-			lo++;
-			hi--;
-		}
-	}
-	return (hi);
-}
-
-/**
- * quicksort - sorts a partition of an array of integers
- * @array: array to sort
- * @lo: lowest index of the partition to sort
- * @hi: highest index of the partition to sort
- * @size: size of the array
+ * @array: ptr to array
+ * @size: size of array
  *
  * Return: void
  */
-void quicksort(int *array, ssize_t lo, ssize_t hi, size_t size)
-{
-	ssize_t pivot;
 
-	if (lo < hi)
-	{
-		pivot = partition(array, lo, hi, size);
-		quicksort(array, lo, pivot, size);
-		quicksort(array, pivot + 1, hi, size);
-
-	}
-}
-
-/**
- * quick_sort_hoare - sorts an array of integers in ascending order using the
- * Quick sort algorithm
- * @array: The array to sort
- * @size: The size of the array
- *
- * Return: void
- */
 void quick_sort_hoare(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
-	quicksort(array, 0, size - 1, size);
+	int pivot = array[size - 1];
+	unsigned int i, j;
+	int temp;
+
+	if (size >= 2)
+	{
+		for (i = 0, j = size - 1;; i++, j--)
+		{
+			while (array[i] < pivot)
+				i++;
+			while (pivot < array[j])
+				j--;
+			if (i >= j)
+				break;
+
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+		quick_sort_hoare(array, i);
+		quick_sort_hoare(array + i, size - i);
+		print_array(array, size);
+	}
 }
